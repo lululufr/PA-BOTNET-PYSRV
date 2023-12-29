6524f9389b9547a9d8dd71757225b9cd2517850e
lululufr <lucas.lm.miller@gmail.com>
2023-12-29 15:01:54 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import socket
+
+def main():
+    host = "127.0.0.1"
+    port = 8080
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+
+        print("Server listening on port 8080...")
+
+        conn, addr = s.accept()
+        with conn:
+            print("Connected by", addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(f"Received from Rust: {data.decode()}")
+
+                message = input("Python Server: ")
+                conn.sendall(message.encode())
+
+                if message.lower() == 'quitter':
+                    break
+
+if __name__ == "__main__":
+    main()
