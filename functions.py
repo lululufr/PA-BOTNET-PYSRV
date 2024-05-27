af55428d40ebc203beb5ca30dad570163157a252
lululufr <lucas.lm.miller@gmail.com>
2024-05-27 16:10:06 +0200

    Fix Json 2

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import json
 
 def format_attack_data(type, id, data):
 
-    json_data = json.loads('{"id":"'+str(id)+'","attack":"'+type+'","arg1":"stealth,t5","arg2":"nono","arg3":""}')
+    json_data = json.loads('{"id":"10","attack":"ddos","arg1":"stealth,t5","arg2":"nono","arg3":"dd"}')
 
     json_data.update(data)
 
